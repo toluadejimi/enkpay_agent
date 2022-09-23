49376deb8e2c16cf490a37a40dfa0ccc11732a5d
jimmy <toluadejimi@gmail.com>
2022-09-23 10:56:46 +0100

    added_platformck

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit2c83f1af6103a94a0931b62ecaebcf18
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit2c83f1af6103a94a0931b62ecaebcf18', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit2c83f1af6103a94a0931b62ecaebcf18', 'loadClassLoader'));
