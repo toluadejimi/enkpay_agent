81979e28dca19caf204aa6478a8ad78017b482f6
jimmy <toluadejimi@gmail.com>
2023-03-27 09:36:07 +0100

    push

--- resources/views/pin-verify.blade.php
+++ resources/views/pin-verify.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Untitled</title>
-    <link rel="stylesheet" href="{{url('')}}/{{url('')}}/public/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{url('')}}/public/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
     <link rel="stylesheet" href="{{url('')}}/public/assets/css/Login-Form-Clean.css">
     <link rel="stylesheet" href="{{url('')}}/public/assets/css/styles.css">
