66e495ff57d7058df6cf5faf916b106c847f185e
jimmy <toluadejimi@gmail.com>
2023-03-27 08:36:53 +0100

    push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,12 +44,14 @@
                     <div class="card-body card-body-padding d-flex align-items-center justify-content-between">
                         <div class="ps-lg-1">
                         <div class="d-flex align-items-center justify-content-between">
-                            <p class="mb-0 font-weight-medium me-3 buy-now-text">Free 24/7 customer support, updates, and more with this template!</p>
-                            <a href="https://www.bootstrapdash.com/product/purple-bootstrap-admin-template/?utm_source=organic&utm_medium=banner&utm_campaign=buynow_demo" target="_blank" class="btn me-2 buy-now-btn border-0">Get Pro</a>
+                            <p class="mb-0 font-weight-medium me-3 buy-now-text">Download ENKPAY Mobile app on both App stores, </p>
+                            <a href="https://playstore.com" target="_blank" class="btn me-2 buy-now-btn border-0">Andriod</a>
+                            <a href="https://iosstore.com" target="_blank" class="btn me-2 buy-now-btn border-0">IOS</a>
+
                         </div>
                         </div>
                         <div class="d-flex align-items-center justify-content-between">
-                        <a href="https://www.bootstrapdash.com/product/purple-bootstrap-admin-template/"><i class="mdi mdi-home me-3 text-white"></i></a>
+                        <a href="https://enkwave.com/"><i class="mdi mdi-home me-3 text-white"></i></a>
                         <button id="bannerClose" class="btn border-0 p-0">
                             <i class="mdi mdi-close text-white me-0"></i>
                         </button>
