327493fbcb13b738d6dcf00dce8990f74acfc053
jimmy <toluadejimi@gmail.com>
2023-04-02 12:19:00 +0100

    push

--- public/index.php
+++ public/index.php
@@ -5,6 +5,9 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+ob_start();
+
+
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
